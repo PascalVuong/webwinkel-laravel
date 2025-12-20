f5bf40ee98718ed86d2f8371dff4cb0c06e0d384
Pascal Vuong <pascalvuongcoding@gmail.com>
2025-12-20 23:21:31 +0100

    Categories CRUD compleet (Slice A)

--- app/Filament/Resources/Categories/Schemas/CategoryForm.php
+++ app/Filament/Resources/Categories/Schemas/CategoryForm.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Resources\Categories\Schemas;
 
+use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Schema;
 
 class CategoryForm
@@ -10,7 +11,17 @@ class CategoryForm
     {
         return $schema
             ->components([
-                //
+                TextInput::make('name')
+                    ->required()
+                    ->helperText('The name of the category')
+                    ->placeholder('For example: Assault Rifles')
+                    ->maxLength(255),
+                TextInput::make('slug')
+                    ->label('URL-name (slug)')
+                    ->helperText('Will be used in the URL, for example: "assault-rifles" -> /categories/assault-rifles')
+                    ->placeholder('For example: assault-rifles')
+                    ->required()
+                    ->maxLength(255)
             ]);
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,7 +14,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::updateOrCreate(
+            ['email' => 'admin@test.nl'],
+            [
+                'name' => 'Admin',
+                'password' => Hash::make('admin123'),
+            ]
+            );
 
         User::firstOrCreate(
             ['email' => 'test@example.com'],
